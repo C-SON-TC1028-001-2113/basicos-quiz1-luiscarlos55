cc79f6c55927d3d175d10511ddb76894dbed6755
luiscarlos55 <a01254044@itesm.mx>
2021-08-20 16:45:58 +0000

    Quiz de programación 1

--- assignments/14DistanciaDosPuntos/src/exercise.py
+++ assignments/14DistanciaDosPuntos/src/exercise.py
@@ -1,6 +1,12 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
+    x1 = int(input("Dame x1: "))
+    y1 = int(input("Dame y1: "))
+    x2 = int(input("Dame X2: "))
+    y2 = int(input("Dame y2: "))
+    distancia = (math.hypot(x2-x1, y2-y1))
+    print('distancia = '+str(round(distancia,2)))
 
 if __name__ == '__main__':
     main()
